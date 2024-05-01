820f02dba692cb3b6cc01487e9fe71538d68da43
Lblzr <itsgdblazar69@gmail.com>
2024-05-01 11:47:54 -0700

    STABLE: Changed font size, again. 14 -> 12. Back to the default.

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMonoNL Nerd Font:pixelsize=14:antialias=true:autohint=true:antialias=true"; // pixelsize=12 ?
+static char *font = "JetBrainsMonoNL Nerd Font:pixelsize=12:antialias=true:autohint=true:antialias=true"; // pixelsize=12 ?
 static int borderpx = 1;
 
 /*
